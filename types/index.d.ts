361d5acd0bcfcfd2f2b448ec0993935c040efe6b
jzlai <jun-zhe.lai@weltn24.de>
2017-11-30 13:30:42 +0100

    fix typing

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,32 +2,26 @@
 // Project: github.com/WeltN24/aws-lambda-router
 
 declare namespace awsLambdaRouter {
-    function handler(routeConfig: RouteConfig);
-    function extractEventProcessorMapping(routeConfig: RouteConfig);
-}
-
-interface ErrorMapping {
-    subject: string;
-    action: any;
+    function handler(routeConfig: RouteConfig): any;
 }
 
 interface ProxyIntegrationRoute {
     path: string;
     method: string;
-    action: Function;
+    action?: (request: any) => any;
 }
 
 interface ProxyIntegrationConfig {
     cors: boolean;
     routes: ProxyIntegrationRoute[];
     debug?: boolean;
-    errorMapping: ErrorMapping;
-    defaultHeaders: string;
+    errorMapping?: any;
+    defaultHeaders?: string;
 }
 
 interface SnsRoute {
-    subject: string;
-    action: Function;
+    subject: any;
+    action?: (sns: any) => any;
 }
 
 interface SnsConfig {
@@ -39,6 +33,3 @@ export interface RouteConfig {
     proxyIntegration: ProxyIntegrationConfig;
     sns: SnsConfig;
 }
-
-
-
